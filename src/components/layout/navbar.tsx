67be9fae7994be9eead4337b3de99898968d07ca
Adil Sayed <adilsayed@Adils-MacBook-Pro.local>
2026-08-07 16:50:58 +0530

    Fix right navbar padding to match top and bottom padding (8px) around Schedule a Demo button

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -62,7 +62,7 @@ const Navbar = () => {
         "lg:fixed lg:inset-x-0 lg:top-[var(--banner-height,0px)] lg:translate-y-[18px]",
       )}
     >
-      <div className="lg:bg-popover/90 relative z-50 container grid grid-cols-2 lg:grid-cols-[1fr_auto_1fr] h-[var(--header-height)] items-center px-4 lg:px-7 lg:h-[58px] lg:rounded-full lg:border lg:border-border/70 lg:backdrop-blur-md">
+      <div className="lg:bg-popover/90 relative z-50 container grid grid-cols-2 lg:grid-cols-[1fr_auto_1fr] h-[var(--header-height)] items-center px-4 lg:pl-6 lg:pr-2 lg:h-[54px] lg:rounded-full lg:border lg:border-border/70 lg:backdrop-blur-md">
         <div className="flex items-center justify-start">
           <Logo className="shrink-0" />
         </div>
@@ -97,7 +97,7 @@ const Navbar = () => {
               key={button.label}
               size="sm"
               variant={button.variant}
-              className="rounded-full font-semibold px-4.5 text-xs md:text-sm h-9"
+              className="rounded-full font-semibold px-5 text-xs md:text-sm h-9.5"
               asChild
             >
               <Link href={button.href}>{button.label}</Link>
